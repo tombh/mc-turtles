ce44450c5e45a3bbf74e7ec2232904c801387d40
Katja Durrani <mail@kdurrani.co.uk>
2014-03-05 02:01:46 +0000

    Two classes for basic turtle movements - buggy and no error-handling ;)

---
+++ turtlecraft.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.append("..")
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+from turtlemc import Turtle
+
+class Turtlecraft:
+    
+    BLOCKTYPES = ['GRASS','AIR','DIRT','STONE','TNT','GOLD_ORE','LAVA','MELON']
+
+
+    def __init__(self,x=0,y=1,z=0,dir=0,block = 'GRASS'):
+        self.Turtle = Turtle(x,y,z,dir,block)
+        self.mc = minecraft.Minecraft.create()
+    
+    def fd(self,steps):
+        self.Turtle.forward(steps)
+        if self.Turtle._pendown:
+            xpos,y,zpos = self.Turtle.getPos()
+            blocktype = self.getBlockString(self.Turtle.getBlockType())
+            for x,z in self.Turtle._coords:
+                self.mc.setBlock(x,y,z,blocktype)
+
+    def lt(self,degrees):
+        self.Turtle.turnBy(degrees*(-1))
+
+    def rt(self,degrees):
+        self.Turtle.turnBy(degrees)
+
+    def pu(self):
+        self.Turtle._pendown = False
+
+    def pd(self):
+        self.Turtle._pendown = True
+
+    def setBT(self, blocktype):
+        self.Turtle.setBlockType(blocktype)
+
+    def getBlockString(self, blocktype):
+            return {
+                'GRASS': block.GRASS,
+                'AIR': block.AIR,
+                'LAVA': block.LAVA,
+                'MELON': block.MELON,
+                'TNT': block.TNT,
+                'STONE': block.STONE,
+                'DIRT': block.DIRT,
+                'GOLD_ORE': block.GOLD_ORE
+                 }.get(blocktype, block.GRASS)   
+
+    def testTC(self):
+        self.mc.postToChat("Connection working")
---
+++ turtlemc.py
@@ -0,0 +1,99 @@
+import sys
+sys.path.append("..")
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import math
+
+
+class Turtle:
+
+    def __init__(self,x=0,y=1,z=0,dir=0,block='GRASS'):
+        self._tilepos = [int(math.floor(x)),int(math.floor(y)),int(math.floor(z))]
+        self._pos = [x,y,z]
+        self._dir = dir
+        self._blocktype = block
+        self._pendown = True
+
+    def setTilePos(self,x,y,z):
+        self._tilepos = [int(math.floor(x)),int(math.floor(y)),int(math.floor(z))]
+
+    def getTilePos(self):
+        return self._tilepos
+
+    def setPos(self,x,y,z):
+        self._pos = [x,y,z]
+
+    def getPos(self):
+        return self._pos
+    
+    #set Direction of Turtles in degrees
+    def setDir(self, dir):
+        self._dir = dir
+
+    def turnBy(self,degree):
+        dircurr = self.getDir()
+        dirnew = dircurr + degree
+        if dirnew >= 360:
+            dirnew = dirnew - 360
+        if dirnew < 0:
+            dirnew = dirnew + 360
+        self.setDir(dirnew)
+
+    def getDir(self):
+        return self._dir
+
+    #todo: give option of either id or name
+    def setBlockType(self, block):
+        self._blocktype = block
+
+    def getBlockType(self):
+        return self._blocktype
+
+    def pendown(self):
+        self._pendown = True
+
+    def pendown(self):
+        self._pendown = False
+
+    def forward(self, steps):
+        startpos = self.getPos()        
+        x,y,z = startpos
+        dir = self.getDir()
+        # calculating the net forward movement on x-axis
+        if self._dir == 90:
+            xdiff = 0        
+            zdiff = steps
+        elif self._dir == 270:
+            xdiff = 0
+            zdiff = steps*(-1)
+        else:
+            xdiff = math.cos(math.radians(dir))*steps
+            zdiff = math.sin(math.radians(dir))*steps
+        # This is going to contain the tiles the turtle passes through
+        # We are only recording x and z coordinates, y stays the same
+        if self._pendown:
+            coords = []
+            if xdiff == steps:
+                for i in range(steps):
+                    coords.append((x+i,z))       
+            elif xdiff == 0:
+                for i in range(steps): 
+                    coords.append((x,z+i))
+            else:
+                # calculate coordinates passed for each x-step forward
+                xsteps = math.ceil(xdiff)
+                zdiffunit = math.tan(math.radians(dir))
+                for i in range(int(xsteps)):
+                    coords.append((x+i,math.ceil(i*zdiffunit)))
+            self._coords = coords
+              
+        newx = x + xdiff
+        newz = z + zdiff
+        self._pos = [newx,y,newz]
+        self._tilepos = [int(math.floor(newx)), y, int(math.floor(newz))]
+        
+               
+    
+    
+    
+
